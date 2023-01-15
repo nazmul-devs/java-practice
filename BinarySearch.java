7cc57578b0083a96d1a22db36787501ef1bc7214
nazmulhosens <nazmulhosenm668@gmail.com>
2023-01-15 22:54:36 +0600

    try linkedList

---
+++ BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int arrLenght, start, end;
+        arrLenght= nums.length;
+        start=nums[0];
+        end = nums[arrLenght/2-1];
+        int targetNum= 4;
+        if(targetNum <end){
+            
+        }
+
+
+        System.out.println(start);
+        System.out.println(end);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+    }
+}
